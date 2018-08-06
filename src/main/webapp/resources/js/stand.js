830f4ac5c247c1417bc47d02d24ee4c208ba83ed
crown <alena_demi01@mail.ru>
2018-08-06 17:23:02 +0300

    css

--- src/main/webapp/resources/js/stand.js
+++ src/main/webapp/resources/js/stand.js
@@ -1,9 +1,10 @@
-
-$(document).ready(function () {
+/*$(document).ready(function () {
+    console.log("in script");
     if(typeof(EventSource) !== "undefined") {
-        var source = new EventSource("http://localhost:8081/advertising/stand/connection");
-       source.onmessage = function() {
+        var source = new EventSource('http://localhost:8081/advertising/stand/connection');
+        source.onmessage = function(event) {
             location.reload();
         };
     }
-})
+});*/
+
